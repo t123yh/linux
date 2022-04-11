57f3bafb20563ba4a39f0e0793948772d80476c0
Yunhao Tian <t123yh.xyz@gmail.com>
2022-04-11 11:34:40 +0800

    Add rotary button support

--- drivers/mfd/smartcross-fpctl.c
+++ drivers/mfd/smartcross-fpctl.c
@@ -24,7 +24,7 @@
 struct smartcross_fpctl_priv {
 	struct regmap *regmap;
 	struct gpio_desc *irq;
-    struct device *dev;
+	struct device *dev;
 	struct rc_dev *rc_dev;
 	struct backlight_device *backlight;
 	struct input_dev *rotary;
@@ -48,6 +48,7 @@ struct smartcross_fpctl_priv {
 #define FPCTL_REG_IR_RX3 37
 #define FPCTL_REG_ROT_LOW 38
 #define FPCTL_REG_ROT_HIGH 39
+#define FPCTL_REG_BUTTON_VALUE 40
 
 #define FPCTL_INT_IR_RECEIVED_SHIFT 0
 #define FPCTL_INT_IR_RECEIVED (1 << FPCTL_INT_IR_RECEIVED_SHIFT)
@@ -55,6 +56,8 @@ struct smartcross_fpctl_priv {
 #define FPCTL_INT_IR_REPEAT (1 << FPCTL_INT_IR_REPEAT_SHIFT)
 #define FPCTL_INT_ROTARY_SHIFT 2
 #define FPCTL_INT_ROTARY (1 << FPCTL_INT_ROTARY_SHIFT)
+#define FPCTL_INT_BUTTON_SHIFT 3
+#define FPCTL_INT_BUTTON (1 << FPCTL_INT_BUTTON_SHIFT)
 
 static int smartcross_bl_get_brightness(struct backlight_device *bl)
 {
@@ -82,11 +85,11 @@ static const struct backlight_ops smartcross_bl_ops = {
 };
 
 static int smartcross_fpled_brightness_set(struct led_classdev *cdev,
-				     enum led_brightness brightness)
+					   enum led_brightness brightness)
 {
 	struct led_classdev_mc *mc_cdev = lcdev_to_mccdev(cdev);
-	struct smartcross_fpctl_priv *priv_data = container_of(mc_cdev, 
-		struct smartcross_fpctl_priv, mcled_cdev);
+	struct smartcross_fpctl_priv *priv_data =
+		container_of(mc_cdev, struct smartcross_fpctl_priv, mcled_cdev);
 	uint8_t dat[3], i;
 
 	led_mc_calc_color_components(mc_cdev, brightness);
@@ -96,31 +99,41 @@ static int smartcross_fpled_brightness_set(struct led_classdev *cdev,
 	return regmap_bulk_write(priv_data->regmap, FPCTL_R, dat, sizeof(dat));
 }
 
-static inline bool is_rev(uint8_t a, uint8_t b) {
+static inline bool is_rev(uint8_t a, uint8_t b)
+{
 	return a == (uint8_t)~b;
 }
 
-static irqreturn_t smartcross_fpctl_irq(int irq_no, void *handle) {
+static irqreturn_t smartcross_fpctl_irq(int irq_no, void *handle)
+{
 	u32 irq = 0;
 	uint8_t ir_data[4];
-	struct smartcross_fpctl_priv* priv_data = (struct smartcross_fpctl_priv*)handle;
+	struct smartcross_fpctl_priv *priv_data =
+		(struct smartcross_fpctl_priv *)handle;
 
 	regmap_read(priv_data->regmap, FPCTL_REG_INT_STATUS, &irq);
-    if (irq & FPCTL_INT_IR_RECEIVED) {
-        dev_dbg(priv_data->dev, "IR Recv\n");
-		regmap_bulk_read(priv_data->regmap, FPCTL_REG_IR_RX0, ir_data, sizeof(ir_data));
-		if (is_rev(ir_data[0], ir_data[1]) && is_rev(ir_data[2], ir_data[3])) {
+	if (irq & FPCTL_INT_IR_RECEIVED) {
+		dev_dbg(priv_data->dev, "IR Recv\n");
+		regmap_bulk_read(priv_data->regmap, FPCTL_REG_IR_RX0, ir_data,
+				 sizeof(ir_data));
+		if (is_rev(ir_data[0], ir_data[1]) &&
+		    is_rev(ir_data[2], ir_data[3])) {
 			// NEC Protocol
-			rc_keydown(priv_data->rc_dev, RC_PROTO_NEC, RC_SCANCODE_NEC(ir_data[0], ir_data[2]), 0);
+			rc_keydown(priv_data->rc_dev, RC_PROTO_NEC,
+				   RC_SCANCODE_NEC(ir_data[0], ir_data[2]), 0);
 		} else if (is_rev(ir_data[2], ir_data[3])) {
 			// NECX Protocol
-			rc_keydown(priv_data->rc_dev, RC_PROTO_NECX, RC_SCANCODE_NECX(ir_data[0] << 8 | ir_data[1], ir_data[2]), 0);
+			rc_keydown(priv_data->rc_dev, RC_PROTO_NECX,
+				   RC_SCANCODE_NECX(ir_data[0] << 8 |
+							    ir_data[1],
+						    ir_data[2]),
+				   0);
 		}
-    }
-    if (irq & FPCTL_INT_IR_REPEAT) {
-        dev_dbg(priv_data->dev, "IR Repeat\n");
+	}
+	if (irq & FPCTL_INT_IR_REPEAT) {
+		dev_dbg(priv_data->dev, "IR Repeat\n");
 		rc_repeat(priv_data->rc_dev);
-    }
+	}
 	if (irq & FPCTL_INT_ROTARY) {
 		int16_t pos, delta;
 		regmap_bulk_read(priv_data->regmap, FPCTL_REG_ROT_LOW, &pos, 2);
@@ -129,24 +142,32 @@ static irqreturn_t smartcross_fpctl_irq(int irq_no, void *handle) {
 		input_sync(priv_data->rotary);
 		priv_data->rotary_last_pos = pos;
 	}
+	if (irq & FPCTL_INT_BUTTON) {
+		int val;
+		regmap_read(priv_data->regmap, FPCTL_REG_BUTTON_VALUE, &val);
+		input_report_key(priv_data->rotary, BTN_0, val);
+		input_sync(priv_data->rotary);
+	}
 
-    regmap_write(priv_data->regmap, FPCTL_REG_INT_ACK, irq);
+	regmap_write(priv_data->regmap, FPCTL_REG_INT_ACK, irq);
 
 	return IRQ_HANDLED;
 }
 
-static bool smartcross_fpctl_readable_register(struct device *dev, unsigned int reg)
+static bool smartcross_fpctl_readable_register(struct device *dev,
+					       unsigned int reg)
 {
 	switch (reg) {
 	case 0 ... 5:
-    case 32 ... 39:
+	case 32 ... 40:
 		return true;
 	default:
 		return false;
 	}
 }
 
-static bool smartcross_fpctl_writeable_register(struct device *dev, unsigned int reg)
+static bool smartcross_fpctl_writeable_register(struct device *dev,
+						unsigned int reg)
 {
 	switch (reg) {
 	case 0 ... 5:
@@ -156,10 +177,11 @@ static bool smartcross_fpctl_writeable_register(struct device *dev, unsigned int
 	}
 }
 
-static bool smartcross_fpctl_volatile_register(struct device* dev, unsigned int reg)
+static bool smartcross_fpctl_volatile_register(struct device *dev,
+					       unsigned int reg)
 {
 	switch (reg) {
-	case 33 ... 39:
+	case 33 ... 40:
 		return true;
 	default:
 		return false;
@@ -167,33 +189,33 @@ static bool smartcross_fpctl_volatile_register(struct device* dev, unsigned int 
 }
 
 static const struct regmap_config smartcross_fpctl_regmap = {
-	.reg_bits		= 8,
-	.val_bits		= 8,
+	.reg_bits = 8,
+	.val_bits = 8,
 
-	.max_register		= 39,
-	.readable_reg		= smartcross_fpctl_readable_register,
-	.writeable_reg		= smartcross_fpctl_writeable_register,
-	.volatile_reg       = smartcross_fpctl_volatile_register,
-	.cache_type			=  REGCACHE_RBTREE,
+	.max_register = 40,
+	.readable_reg = smartcross_fpctl_readable_register,
+	.writeable_reg = smartcross_fpctl_writeable_register,
+	.volatile_reg = smartcross_fpctl_volatile_register,
+	.cache_type = REGCACHE_RBTREE,
 };
 
 static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
-			   const struct i2c_device_id *id)
+				      const struct i2c_device_id *id)
 {
 	struct smartcross_fpctl_priv *priv_data;
 	struct backlight_properties props;
-	char* name;
+	char *name;
 	int ret = 0, val;
 	struct led_init_data ws2812_init_data = {
-		.default_label = "ws2812",
-		.devicename = dev_name(&client->dev)
+		.default_label = "ws2812", .devicename = dev_name(&client->dev)
 	};
 
-	priv_data = devm_kzalloc(&client->dev, sizeof(struct smartcross_fpctl_priv), GFP_KERNEL);
+	priv_data = devm_kzalloc(
+		&client->dev, sizeof(struct smartcross_fpctl_priv), GFP_KERNEL);
 	if (!priv_data)
 		return -ENOMEM;
 	i2c_set_clientdata(client, priv_data);
-    priv_data->dev = &client->dev;
+	priv_data->dev = &client->dev;
 
 	priv_data->irq = devm_gpiod_get(&client->dev, "irq", GPIOD_IN);
 	if (IS_ERR(priv_data->irq)) {
@@ -203,25 +225,28 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 		return ret;
 	}
 
-	priv_data->regmap = devm_regmap_init_i2c(client, &smartcross_fpctl_regmap);
+	priv_data->regmap =
+		devm_regmap_init_i2c(client, &smartcross_fpctl_regmap);
 	if (IS_ERR(priv_data->regmap)) {
 		ret = PTR_ERR(priv_data->regmap);
 		dev_err(&client->dev, "regmap_init() failed: %d\n", ret);
 		return ret;
 	}
 
-    ret = regmap_read(priv_data->regmap, FPCTL_REG_ID, &val);
-    if (ret < 0) {
-        dev_err(&client->dev, "Failed to read fpctl ID register: %d\n", ret);
-        return ret;
-    }
+	ret = regmap_read(priv_data->regmap, FPCTL_REG_ID, &val);
+	if (ret < 0) {
+		dev_err(&client->dev, "Failed to read fpctl ID register: %d\n",
+			ret);
+		return ret;
+	}
 
-    if (val != 0xAB) {
-        dev_err(&client->dev, "Invalid fpctl device ID: %02X\n", val);
-        return -EINVAL;
-    }
+	if (val != 0xAB) {
+		dev_err(&client->dev, "Invalid fpctl device ID: %02X\n", val);
+		return -EINVAL;
+	}
 
-	priv_data->rc_dev = devm_rc_allocate_device(&client->dev, RC_DRIVER_SCANCODE);
+	priv_data->rc_dev =
+		devm_rc_allocate_device(&client->dev, RC_DRIVER_SCANCODE);
 	if (IS_ERR(priv_data->rc_dev)) {
 		ret = PTR_ERR(priv_data->rc_dev);
 		dev_err(&client->dev, "Failed to allocate rc_dev %d\n", ret);
@@ -229,7 +254,8 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 	}
 
 	priv_data->rc_dev->map_name = RC_MAP_EMPTY;
-	priv_data->rc_dev->allowed_protocols = RC_PROTO_BIT_NECX | RC_PROTO_BIT_NEC;
+	priv_data->rc_dev->allowed_protocols =
+		RC_PROTO_BIT_NECX | RC_PROTO_BIT_NEC;
 	priv_data->rc_dev->input_phys = DRIVER_NAME "/input-rc";
 	priv_data->rc_dev->driver_name = DRIVER_NAME;
 #define NAME_LEN 20
@@ -241,8 +267,9 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 
 	ret = devm_rc_register_device(&client->dev, priv_data->rc_dev);
 	if (ret < 0) {
-		dev_err(&client->dev, "Failed to register RC device: %d\n", ret);
-        return -EINVAL;
+		dev_err(&client->dev, "Failed to register RC device: %d\n",
+			ret);
+		return -EINVAL;
 	}
 
 	memset(&props, 0, sizeof(struct backlight_properties));
@@ -250,19 +277,21 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 	props.brightness = 8;
 	props.max_brightness = 17;
 
-	priv_data->backlight = devm_backlight_device_register(&client->dev, "smartcross-backlight",
-				    &client->dev, priv_data,
-				    &smartcross_bl_ops, &props);
+	priv_data->backlight = devm_backlight_device_register(
+		&client->dev, "smartcross-backlight", &client->dev, priv_data,
+		&smartcross_bl_ops, &props);
 	if (IS_ERR(priv_data->backlight)) {
 		ret = PTR_ERR(priv_data->backlight);
-		dev_err(&client->dev, "Failed to register backlight: %d\n", ret);
+		dev_err(&client->dev, "Failed to register backlight: %d\n",
+			ret);
 		return ret;
 	}
 
 	priv_data->rotary = devm_input_allocate_device(&client->dev);
 	if (IS_ERR(priv_data->rotary)) {
 		ret = PTR_ERR(priv_data->rotary);
-		dev_err(&client->dev, "Failed to allocate rotary encoder: %d\n", ret);
+		dev_err(&client->dev, "Failed to allocate rotary encoder: %d\n",
+			ret);
 		return ret;
 	}
 
@@ -271,10 +300,12 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 	priv_data->rotary->name = name;
 	priv_data->rotary->id.bustype = BUS_I2C;
 	input_set_capability(priv_data->rotary, EV_REL, REL_X);
+	input_set_capability(priv_data->rotary, EV_KEY, BTN_0);
 
 	ret = input_register_device(priv_data->rotary);
 	if (ret) {
-		dev_err(&client->dev, "Failed to register rotary encoder device: %d\n", ret);
+		dev_err(&client->dev,
+			"Failed to register rotary encoder device: %d\n", ret);
 		return ret;
 	}
 
@@ -287,54 +318,65 @@ static int smartcross_fpctl_i2c_probe(struct i2c_client *client,
 	priv_data->mcled_cdev.subled_info = priv_data->subled_info;
 	priv_data->mcled_cdev.num_colors = ARRAY_SIZE(priv_data->subled_info);
 	priv_data->mcled_cdev.led_cdev.max_brightness = 255;
-	priv_data->mcled_cdev.led_cdev.brightness_set_blocking = smartcross_fpled_brightness_set;
-	ret = devm_led_classdev_multicolor_register_ext(&client->dev, &priv_data->mcled_cdev,
-							&ws2812_init_data);
+	priv_data->mcled_cdev.led_cdev.brightness_set_blocking =
+		smartcross_fpled_brightness_set;
+	ret = devm_led_classdev_multicolor_register_ext(
+		&client->dev, &priv_data->mcled_cdev, &ws2812_init_data);
 	if (ret) {
-		dev_err(&client->dev, "Failed to register LED device: %d\n", ret);
+		dev_err(&client->dev, "Failed to register LED device: %d\n",
+			ret);
 		return ret;
 	}
-	
-	regmap_bulk_read(priv_data->regmap, FPCTL_REG_ROT_LOW, &priv_data->rotary_last_pos, 2);
-    regmap_write(priv_data->regmap, FPCTL_REG_INT_ACK, 0xFF);
-    regmap_write(priv_data->regmap, FPCTL_REG_INT_ENABLE, FPCTL_INT_IR_RECEIVED | FPCTL_INT_IR_REPEAT | FPCTL_INT_ROTARY);
 
-	ret = devm_request_threaded_irq(&client->dev, gpiod_to_irq(priv_data->irq),
-					NULL, smartcross_fpctl_irq,
+	regmap_bulk_read(priv_data->regmap, FPCTL_REG_ROT_LOW,
+			 &priv_data->rotary_last_pos, 2);
+	regmap_write(priv_data->regmap, FPCTL_REG_INT_ACK, 0xFF);
+	regmap_write(priv_data->regmap, FPCTL_REG_INT_ENABLE,
+		     FPCTL_INT_IR_RECEIVED | FPCTL_INT_IR_REPEAT |
+			     FPCTL_INT_ROTARY | FPCTL_INT_BUTTON);
+
+	ret = devm_request_threaded_irq(&client->dev,
+					gpiod_to_irq(priv_data->irq), NULL,
+					smartcross_fpctl_irq,
 					IRQF_TRIGGER_RISING | IRQF_ONESHOT,
-					"smartcross-fpctl-irq", priv_data);				
+					"smartcross-fpctl-irq", priv_data);
 	if (ret < 0) {
 		dev_err(&client->dev, "Failed to request irq: %d\n", ret);
 		return ret;
 	}
 
-	dev_info(&client->dev, "SmartCross front panel controller initialized\n");
+	dev_info(&client->dev,
+		 "SmartCross front panel controller initialized\n");
 
 	return 0;
 }
 
-void smartcross_fpctl_i2c_shutdown(struct i2c_client *client) {
+void smartcross_fpctl_i2c_shutdown(struct i2c_client *client)
+{
 	struct smartcross_fpctl_priv *priv_data = dev_get_drvdata(&client->dev);
-	uint8_t data[4] = {0,0,0,0};
+	uint8_t data[4] = { 0, 0, 0, 0 };
 
 	// disable all LED and backlight
 	regmap_bulk_write(priv_data->regmap, FPCTL_BL, data, sizeof(data));
 }
 
-int smartcross_fpctl_i2c_remove(struct i2c_client *client) {
+int smartcross_fpctl_i2c_remove(struct i2c_client *client)
+{
 	smartcross_fpctl_i2c_shutdown(client);
 	return 0;
 }
 
 static const struct of_device_id smartcross_fpctl_of_match[] = {
-	{ .compatible = "smartcross,fpctl-v1", },
+	{
+		.compatible = "smartcross,fpctl-v1",
+	},
 	{},
 };
 
 MODULE_DEVICE_TABLE(of, smartcross_fpctl_of_match);
 
 static const struct i2c_device_id smartcross_fpctl_i2c_id[] = {
-	{"smartcross-fpctl", 0},
+	{ "smartcross-fpctl", 0 },
 	{}
 };
 
